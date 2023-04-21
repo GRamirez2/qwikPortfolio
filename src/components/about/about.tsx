6e74229c4a332dc8fb878a694b0aa746e4a2ee9e
George Ramirez <george.ramirez2@gmail.com>
2023-04-21 11:27:59 -0500

    commented out unused css modules

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import styles from './about.module.css';
+// import styles from './about.module.css';
 
 export default component$(() => {
     return (
