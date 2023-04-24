5ccede907e2fd725450ef96f953c4cf208b6f0f2
George Ramirez <george.ramirez2@gmail.com>
2023-04-24 13:17:17 -0500

    added meta data and link to images

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,8 +17,44 @@ export const head: DocumentHead = {
   title: 'George Ramirez | Software Engineer',
   meta: [
     {
-      name: 'George Ramirez info',
-      content: 'Bio and stuffs',
+      name: 'keywords',
+      content: 'george ramirez, angular, node, manager, team lead, javascript, js',
     },
+    {
+      name: 'description',
+      content: 'George Ramirez software engineer bio and sample projects',
+    },
+    {
+      name: 'author',
+      content: 'George Ramirez',
+    },
+    {
+      property: 'og:locale',
+      content: 'en_US'
+    },
+    {
+      property: 'og:title',
+      content: 'George Ramirez\'s software engineer biography'
+    },
+    {
+      property: 'og:type',
+      content: 'portfolio'
+    },
+    {
+      property: 'og:url',
+      content: 'georgeramirez.me'
+    },
+    {
+      property: 'og:image',
+      content: 'https://gr.manage-my.site/George_Ramirez_preview.png'
+    },
+    {
+      property: 'og:image:width',
+      content: '1200'
+    },
+    {
+      property: 'og:image:height',
+      content: '500'
+    }
   ],
 };
